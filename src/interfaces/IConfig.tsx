4297ef5847870fd2550d5225fc0c7e11d8257c5b
mcarr823 <136939846+mcarr823@users.noreply.github.com>
2024-06-28 10:52:21 +1000

    Added configuration file interface for potential settings and customizations to be implemented

---
+++ src/interfaces/IConfig.tsx
@@ -0,0 +1,38 @@
+/**
+ * Interface representing the app's config file: data/config.json
+ */
+export default interface IConfig{
+
+    // Number of attempts which the player is given to guess the word.
+    attempts: number;
+
+    // Number of letters in the word to guess.
+    letters: number;
+
+    // If true, color the keyboard differently for letters which
+    // have already been guessed.
+    // Set to false to make the game more difficult.
+    keyColor: boolean;
+
+    // If true, prompt the user for confirmation if they've already
+    // tried to use a given letter.
+    // This is in case they accidentally guess a letter which has
+    // already been crossed off.
+    warnAlreadyAttempted: boolean;
+
+    // If true, use a database instance to retrieve the word list.
+    // If false, store words in data/${length}.json instead.
+    useDatabase: boolean;
+
+    // Type of database to use.
+    // eg. MongoDB, Postgres, MySQL
+    dbType: string;
+
+    // IP address or hostname of the database host.
+    // eg. 192.168.1.10, or pi.mydomain.com
+    dbHost: string;
+
+    // Port through which to connect to the database.
+    // eg. 27017 for MongoDB
+    dbPort: number;
+}
