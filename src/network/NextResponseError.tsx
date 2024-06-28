e3b84a38ea83f0350d62117cdc65d87683011502
mcarr823 <136939846+mcarr823@users.noreply.github.com>
2024-06-28 10:55:17 +1000

    Added network response wrapper classes

---
+++ src/network/NextResponseError.tsx
@@ -0,0 +1,23 @@
+import { NextResponse } from 'next/server'
+
+/**
+ * Represents an unsuccessful HTTP request.
+ * 
+ * Sends back an error message in JSON.
+ * 
+ * @param error Error message to return with the HTTP response
+ * @param statusCode Status code of the response. Defaults to a 500 error
+ * @returns NextResponse
+ */
+export default function NextResponseError(
+    error: string,
+    statusCode: number = 500
+): NextResponse<INextResponseError>{
+    const json: INextResponseError = { success:false, error }
+    return NextResponse.json(json, { status: statusCode })
+}
+
+export interface INextResponseError{
+    success: boolean;
+    error: string;
+}
---
+++ src/network/NextResponseSuccess.tsx
@@ -0,0 +1,27 @@
+import { NextResponse } from 'next/server'
+
+/**
+ * Represents a successful HTTP request.
+ * 
+ * Used to send back JSON data to a client inside of a HTTP 200 response.
+ * 
+ * @param data Optional data to send back to the client along with the
+ * success message
+ * @returns NextResponse
+ */
+export default function NextResponseSuccess(
+    data?: any
+): NextResponse<INextResponseSuccess> {
+    var json: INextResponseSuccess
+    if (data){
+        json = { success:true, data }
+    }else{
+        json = { success:true }
+    }
+    return NextResponse.json(json)
+}
+
+export interface INextResponseSuccess{
+    success: boolean;
+    data?: any;
+}
