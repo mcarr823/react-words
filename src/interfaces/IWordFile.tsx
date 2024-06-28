0eb6e47fd0c81c02e98dd6395b3208e998a671ad
mcarr823 <136939846+mcarr823@users.noreply.github.com>
2024-06-28 15:26:05 +1000

    Created an interface to define the file format of dictionaries, updated example files to match

---
+++ src/interfaces/IWordFile.tsx
@@ -0,0 +1,10 @@
+/**
+ * Specifies the format which a file in /data/words/
+ * must adhere to.
+ */
+export default interface IWordFile{
+
+    // An array containing zero or more words.
+    words: Array<string>;
+
+}
