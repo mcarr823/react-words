389fdb914e8172b56d88194bb76a4771c9bc01f0
mcarr823 <136939846+mcarr823@users.noreply.github.com>
2024-06-28 10:51:05 +1000

    Updated layout to add navbar and bootstrap styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,46 @@
+import Link from 'next/link'
+import 'bootstrap/dist/css/bootstrap.css';
+
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
+
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <div className="container-fluid">
+            <Link className="navbar-brand" href="#">Words</Link>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <NavLink text="Home" href="/"/>
+                <NavLink text="About" href="/about"/>
+                <NavLink text="Play" href="/play"/>
+                <NavLink text="Setup" href="/settings"/>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        <main className="container pt-3">
+          {children}
+        </main>
+      </body>
     </html>
   );
+}
+
+function NavLink(args : INavLinkArgs){
+  const { href, text } = args;
+  return (
+    <li className="nav-item">
+      <Link className="nav-link" href={href}>{text}</Link>
+    </li>
+  )
+}
+
+interface INavLinkArgs{
+  text: string;
+  href: string;
 }
