c3b56ce2835959ffc0eda366e082f407a772d55a
mcarr823 <136939846+mcarr823@users.noreply.github.com>
2024-07-07 20:48:40 +1000

    Added XML and CSV to DatabaseType enum

--- src/enums/DatabaseType.tsx
+++ src/enums/DatabaseType.tsx
@@ -2,7 +2,9 @@ import KeyValuePair from "classes/KeyValuePair";
 
 export default class DatabaseType{
 
-    static JSON = new KeyValuePair({ key: "json", value: "JSON (only suitable for smaller dictionaries)" })
+    static JSON = new KeyValuePair({ key: "json", value: "JSON" })
+    static XML = new KeyValuePair({ key: "xml", value: "XML" })
+    static CSV = new KeyValuePair({ key: "csv", value: "CSV" })
     static MONGO = new KeyValuePair({ key: "mongo", value: "MongoDB" })
     static MYSQL = new KeyValuePair({ key: "mysql", value: "MySQL" })
     static POSTGRES = new KeyValuePair({ key: "postgres", value: "PostgreSQL" })
@@ -10,6 +12,8 @@ export default class DatabaseType{
     static all(): Array<KeyValuePair>{
         return [
             DatabaseType.JSON,
+            DatabaseType.XML,
+            DatabaseType.CSV,
             DatabaseType.MONGO,
             DatabaseType.MYSQL,
             DatabaseType.POSTGRES
