be3a39262adee09966da1bd09b516fc0ddcb11ec
mcarr823 <136939846+mcarr823@users.noreply.github.com>
2024-08-05 18:21:22 +1000

    JSON driver now supports a second format. Added more extensive unit tests for the json driver

--- src/database/json/JsonDriver.test.tsx
+++ src/database/json/JsonDriver.test.tsx
@@ -5,7 +5,7 @@
 import JsonDriver from './JsonDriver';
 import {expect, test} from '@jest/globals';
 
-test('JsonDriver parsing test', async () => {
+test('JsonDriver parsing test - format 1', async () => {
 
     const testJsonData = `{
         "words": [
@@ -31,5 +31,78 @@ test('JsonDriver parsing test', async () => {
     expect(fiveLetterWords.length).toBe(1)
     expect(sixLetterWords.length).toBe(0)
     expect(sevenLetterWords.length).toBe(1)
+
+});
+
+test('JsonDriver parsing test - format 2', async () => {
+
+    const testJsonData = `[
+        "paste",
+        "pest",
+        "testing",
+        "test"
+    ]`
+
+    const driver = new JsonDriver()
+    const wordFile = driver.parseWordFile(testJsonData)
+
+    const words = wordFile.words
+    expect(words.length).toBe(4)
+
+    const fourLetterWords = words.filter(w => w.w_length == 4)
+    const fiveLetterWords = words.filter(w => w.w_length == 5)
+    const sixLetterWords = words.filter(w => w.w_length == 6)
+    const sevenLetterWords = words.filter(w => w.w_length == 7)
+
+    expect(fourLetterWords.length).toBe(2)
+    expect(fiveLetterWords.length).toBe(1)
+    expect(sixLetterWords.length).toBe(0)
+    expect(sevenLetterWords.length).toBe(1)
+
+});
+
+test('JsonDriver parsing test - format 1 failure', async () => {
+
+    // "word" instead of "words"
+    const testJsonData = `{
+        "word": [
+            "paste",
+            "pest",
+            "testing",
+            "test"
+        ]
+    }`
+
+    const driver = new JsonDriver()
+    let failedToParse = false
+    try {
+        driver.parseWordFile(testJsonData)
+    } catch (error) {
+        failedToParse = true
+    }
+
+    expect(failedToParse).toBe(true)
+
+});
+
+test('JsonDriver parsing test - format 2 failure', async () => {
+
+    // Missing close bracket
+    const testJsonData = `[
+        "paste",
+        "pest",
+        "testing",
+        "test"
+    `
+
+    const driver = new JsonDriver()
+    let failedToParse = false
+    try {
+        driver.parseWordFile(testJsonData)
+    } catch (error) {
+        failedToParse = true
+    }
+
+    expect(failedToParse).toBe(true)
 
 });
--- src/database/json/JsonDriver.tsx
+++ src/database/json/JsonDriver.tsx
@@ -8,9 +8,13 @@ import IWordFile from "interfaces/IWordFile"
  * 
  * Expected format:
  * {"words": [ word1, word2, word3, etc. ]}
+ * or
+ * [ word1, word2, word3, etc. ]
  * 
  * eg.
- * {"words": [ "easy", "four", "word"]}
+ * {"words": ["easy", "four", "word"]}
+ * or
+ * ["easy", "four", "word"]
  */
 export default class JsonDriver extends AbstractFlatFileDriver{
 
@@ -21,7 +25,19 @@ export default class JsonDriver extends AbstractFlatFileDriver{
     parseWordFile(data: string): IWordFile{
         
         const json = JSON.parse(data)
-        const strArray = json.words as Array<string>
+
+        let strArray: Array<string>;
+
+        if (Array.isArray(json)){
+            // First format: ["word","word2",etc.]
+            strArray = json as Array<string>;
+        }else if (typeof json === 'object' && 'words' in json){
+            // Second format: {"words":["word", "word2", etc.]}
+            strArray = json.words as Array<string>;
+        }else{
+            throw new Error("Invalid format");
+        }
+
         var w_id = 0
         const words = strArray.map(w_word => {
             w_id += 1
